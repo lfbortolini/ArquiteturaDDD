f9b63be662208ba254eaa93130c2588f742823ab
Luiz Fernando Bortolini <luiz.bortolini@softplan.com.br>
2021-09-05 21:47:11 -0300

    81. Api.Service - LoginService

---
+++ src/Api.Service/Services/LoginService.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces.Service.User;
+using Api.Domain.Repository;
+
+namespace Api.Service.Services
+{
+    public class LoginService : ILoginService
+    {
+        private IUserRepository _repository;
+
+        public LoginService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<object> FindyByLogin(UserEntity user)
+        {
+            if (user != null && !string.IsNullOrWhiteSpace(user.Email))
+            {
+                return await _repository.FindByLogin(user.Email);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
